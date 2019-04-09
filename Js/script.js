475124aa1ea82c33ba6721b12e793d655ff6c69b
Jae423 <45524635+Jae423@users.noreply.github.com>
2019-04-09 11:31:47 -0400

    website

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+
+$('#mint-chocolate').on('click', function() {
+  $('body').css('background', '#1CD396');
+});
+
+$('#strawberry').on('click', function() {
+  $('body').css('background', '#D36E97');
+});
+
+$('#chocolate').on('click', function() {
+  $('body').css('background', '#96614E');
+});
