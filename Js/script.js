7b508f2722dd176cde712557f1e10d06adeec714
Jae423 <45524635+Jae423@users.noreply.github.com>
2019-04-09 11:58:46 -0400

    chris code

--- Js/script.js
+++ Js/script.js
@@ -1,12 +1,27 @@
-
-$('#mint-chocolate').on('click', function() {
-  $('body').css('background', '#1CD396');
+$('#alertButton').on('click', function() {
+	alert('Chris Whong');
 });
 
-$('#strawberry').on('click', function() {
-  $('body').css('background', '#D36E97');
+$('#red-button').on('click', function() {
+  $('body').css('background', 'red');
 });
 
-$('#chocolate').on('click', function() {
-  $('body').css('background', '#96614E');
+$('#green-button').on('click', function() {
+  $('body').css('background', 'green');
+});
+
+$('#blue-button').on('click', function() {
+  $('body').css('background', 'blue');
+});
+
+$('.joes-pizza').on('click', function() {
+  $('#pizza-shop-info').text('Joes Pizza is the best in NYC');
+});
+
+$('.dominos-pizza').on('click', function() {
+  $('#pizza-shop-info').text('Dominos is delicious');
+});
+
+$('.papa-johns').on('click', function() {
+  $('#pizza-shop-info').text('Papa Johns is mediocre');
 });
